53bef193a4ecd0479731c7d340a4c7c81f6375c0
Paulo Sergio <49497668+paulosergiocf@users.noreply.github.com>
2020-11-25 15:40:16 -0300

    Add files via upload

---
+++ UTM/modulos_install.bat
@@ -0,0 +1,7 @@
+@echo "instalando modulos nescessarios para utilização do programa"
+@echo " "
+@echo "--------------------------------"
+@echo " "
+@echo "por favor aguarde"
+pip install PyAutocad
+@echo "instalacao concluida!"
